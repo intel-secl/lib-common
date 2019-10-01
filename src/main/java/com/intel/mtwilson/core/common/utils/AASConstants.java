91a01e78e364efe6f798e451ba4295dbde6065aa
Praveenraj R <praveenraj.r@intel.com>
2019-10-01 20:11:03 +0530

    v1.6/tasks/secure-tls-connection: Created a constants file
    Change-Id: Ic46a7680f5459ec82d6c1e294abb75080e34e7eb

---
+++ src/main/java/com/intel/mtwilson/core/common/utils/AASConstants.java
@@ -0,0 +1,9 @@
+package com.intel.mtwilson.core.common.utils;
+
+public class AASConstants {
+    public static final String CMS_BASE_URL = "cms.base.url";
+    public static final String AAS_API_URL = "aas.api.url";
+    public static final String MC_FIRST_USERNAME = "mc.first.username";
+    public static final String MC_FIRST_PASSWORD = "mc.first.password";
+    public static final String BEARER_TOKEN = "bearer.token";
+}
